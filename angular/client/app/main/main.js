be735b53fc83f95262945134da0c702ce2a1c3f4
Tom Manion <thomaswmanion@gmail.com>
2015-01-22 00:15:20 -0600

    Initial push of Angular Code.

---
+++ angular/client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('angularApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
