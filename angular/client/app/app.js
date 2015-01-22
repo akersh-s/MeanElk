c40d8d2928e7426f0153e24631bff9725929433b
Tom Manion <thomaswmanion@gmail.com>
2015-01-22 01:08:15 -0600

    leaflet map added

--- angular/client/app/app.js
+++ angular/client/app/app.js
@@ -5,7 +5,8 @@ angular.module('angularApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'leaflet-directive'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
--- angular/client/app/main/main.controller.js
+++ angular/client/app/main/main.controller.js
@@ -1,5 +1,31 @@
 'use strict';
 
 angular.module('angularApp').controller('MainCtrl', function ($scope) {
-
+	var dataPoints = [
+                [44.651144316,-89.586260171, 50.5],
+                [44.75, -63.5, 4.8] ];
+	$scope.center = {
+        lat: 39.8282,
+        lng: -98.57,
+        zoom: 4
+    };
+    angular.extend($scope, {
+    	layers: {
+                    baselayers: {
+                        osm: {
+                            name: 'OpenStreetMap',
+                            url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                            type: 'xyz'
+                        }
+                    },
+                    overlays: {
+                        heatmap: {
+                            name: 'Heat Map',
+                            type: 'heatmap',
+                            data: dataPoints,
+                            visible: true
+                        }
+                    }
+                }
+            });
 });
