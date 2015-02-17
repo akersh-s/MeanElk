f48e77e7ac1c01d09259763a65e077b0693cedcc
Akersh <akersh.s@gmail.com>
2015-02-17 01:59:16 -0800

    added a location parameter to the request, should help increase the granularity of the search by a bit

--- angular/server/config/socket/search-tweet.js
+++ angular/server/config/socket/search-tweet.js
@@ -3,7 +3,7 @@ var Twitter = require('twitter');
 var twitter = new Twitter(require('./twitter-auth'));
 
 searchTweet.countWords = function(keywords, socket) {
-	twitter.stream('filter',{track: keywords, language: 'en'}, function(stream) {
+	twitter.stream('filter',{track: keywords, language: 'en', locations: '-122.75,36.8,-121.75,37.8,-74,40,-73,41'}, function(stream) {
 		stream.on('data', function(data) {
 			if (data && data.geo && data.geo.coordinates) {
 				socket.emit('tweetInfo', data.geo.coordinates);
