fd9f2ab805e6e1c40bf8c0ba9e168b21f61c033a
Tom Manion <thomaswmanion@gmail.com>
2015-03-31 14:11:20 -0500

    Added Stanford Sentiment Analysis - Determines if the Tweet is Positive, Neutral, or Negative...

--- back-end/build.sbt
+++ back-end/build.sbt
@@ -8,8 +8,9 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.3.9",
   "com.typesafe.akka" %% "akka-testkit" % "2.3.9" % "test",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test",
-  "org.twitter4j" % "twitter4j-stream" % "4.0.2"
-  
+  "org.twitter4j" % "twitter4j-stream" % "4.0.2",
+  "edu.stanford.nlp" % "stanford-corenlp" % "3.5.1",
+  "edu.stanford.nlp" % "stanford-corenlp" % "3.5.1" classifier "models"
 )
 
 
--- back-end/src/main/scala/com/tempest/a/SentimentActor.scala
+++ back-end/src/main/scala/com/tempest/a/SentimentActor.scala
@@ -1,5 +1,13 @@
 package com.tempest.a
 import akka.actor.{Actor, ActorLogging, Props}
+import edu.stanford.nlp.io._
+import edu.stanford.nlp.ling._
+import edu.stanford.nlp.pipeline._
+import edu.stanford.nlp.trees._
+import edu.stanford.nlp.util._
+import edu.stanford.nlp.sentiment._
+import java.util.Properties
+import scala.collection.JavaConversions._
 
 object SentimentActor {
   val props = Props[SentimentActor]
@@ -15,6 +23,14 @@ class SentimentActor extends Actor with ActorLogging {
   }
   
   def processMessage(message: String) {
-    log.info(message)
+  	val props = new Properties
+  	props.setProperty("annotators", "tokenize, ssplit, pos, lemma, parse, sentiment")
+  	val pipeline = new StanfordCoreNLP(props)
+  	val annotation = pipeline.process(message)
+  	val sentences = annotation.get(classOf[CoreAnnotations.SentencesAnnotation])
+  	sentences.toList.foreach { sentence =>
+  		val sentiment = sentence.get(classOf[SentimentCoreAnnotations.ClassName])
+  		log.info(sentiment.toString() + "\t" + sentence)
+  	}
   }
 }
